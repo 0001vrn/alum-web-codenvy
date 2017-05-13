5a9f13275ddd141cefb05c88e24f427a7ea04659
Varun Thakur <vrnthkr969@gmail.com>
2017-05-13 12:13:38 +0000

    main commit

---
+++ config/env/development.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+	db: 'mongodb://localhost/ducs-alumni-dev',
+	app: {
+		title: 'DUCS Alumni - Development Environment'
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || '1676860182623020',
+		clientSecret: process.env.FACEBOOK_SECRET || 'be2e98cc0ef310d981db0970f4391b17',
+		callbackURL: '/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || 'APP_ID',
+		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || '81mzh5rdizure7',
+		clientSecret: process.env.LINKEDIN_SECRET || 'bU7gCb832QVbG7gY',
+		callbackURL: '/auth/linkedin/callback'
+	},
+	github: {
+		clientID: process.env.GITHUB_ID || 'APP_ID',
+		clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/github/callback'
+	},
+	mailer: {
+		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		options: {
+			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			auth: {
+				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+			}
+		}
+	}
+};
---
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
+	function($scope, $http, $location, Authentication) {
+		$scope.authentication = Authentication;
+
+		// If user is signed in then redirect back home
+		if ($scope.authentication.user) $location.path('/');
+
+		$scope.signup = function() {
+			$http.post('/auth/signup', $scope.credentials).success(function(response) {
+				// If successful we assign the response to the global user model
+				$scope.authentication.user = response;
+
+				// And redirect to the index page
+				$location.path('/');
+			}).error(function(response) {
+				$scope.error = response.message;
+			});
+		};
+
+		$scope.signin = function() {
+			$http.post('/auth/signin', $scope.credentials).success(function(response) {
+				// If successful we assign the response to the global user model
+				$scope.authentication.user = response;
+
+				// And redirect to the index page
+				$location.path('/');
+			}).error(function(response) {
+				$scope.error = response.message;
+			});
+		};
+	}
+]);
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1676860182623020',
+      xfbml      : true,
+      version    : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
